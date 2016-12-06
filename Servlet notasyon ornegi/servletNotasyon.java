935f48740e3041e99e1218281f1d9c3b1e0ad19a
hanifekurnaz <hanifekurnaz@outlook.com>
2016-12-06 11:15:35 +0300

    web xml kullanmadan mesaj yazdirma

---
+++ Servlet notasyon ornegi/servletNotasyon.java
@@ -0,0 +1,19 @@
+package servletNotasyon;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns={"/MerhabaServlet","/HelloServlet"})
+public class servletNotasyon extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws IOException
+	{
+		PrintWriter yaz = response.getWriter();
+		yaz.write("Merhaba Servlet");	
+	}
+}
